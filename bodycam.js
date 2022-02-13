9a79af790b503c2745eb90ca01acd14314f6b425
Tyrope <tyrope@tyrope.nl>
2022-02-13 14:37:25 +0100

    Rename top-right.html to index.html; turn single-use classes into ids, remove unused css class, merge spans to make the HTML code more readable.

--- bodycam.js
+++ bodycam.js
@@ -1,52 +1,50 @@
-        //***edit this only for your information***
-        const player = "M. HIGHTOWER";
-        const agency = "LOS SANTOS POLICE DEPARTMENT";
-        const callsign = "[272]";
-        //***        end edit this only         ***
-    
-        var d,h,m,s,animate;
-        const monthNames = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"];
+// CONFIG START
+const player = "M. HIGHTOWER";
+const agency = "LOS SANTOS POLICE DEPARTMENT";
+const callsign = "[272]";
+// CONFIG ENDS
 
-        function init(){
-            d=new Date();
-            t=d.toLocaleString('en', {timeZoneName:'short'}).split(' ').pop();
-            clock();
-        };
+const monthNames = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"];
+const timezone = Date().toLocaleString('en', {timeZoneName: 'short'}).split(' ')[5];
 
-        function clock(){
-            d=new Date();
-            updateTime();
-            updateDate();
-            $('sec', s);
-            $('min', m);
-            $('hr', h);
-            $('tz', t);
-            $('day', day);
-            $('year', year);
-            $('month', monthNames[month]);
-            $('player', player);
-            $('agency', agency);
-            $('callsign', callsign);
-            animate=setTimeout(clock,100);
-        };
-        
-        function updateDate(){
-            day=d.getDate();
-            month=d.getMonth();
-            year=d.getFullYear();
-        };
-        
-        function updateTime(){
-            h=d.getHours();
-            m=d.getMinutes();
-            s=d.getSeconds();
-        };
+/** Initialize the bodycam overlay with the static information, and start the clock. */
+function init() {
 
-        function $(id,val){
-            if(val<10){
-                val='0'+val;
-            }
-            document.getElementById(id).innerHTML=val;
-        };
+    writeToPage('player', player);
+    writeToPage('agency', agency);
+    writeToPage('callsign', callsign);
+    clock();
+}
 
-        window.onload=init;
+/** Write the current time/day to the overlay every 100ms. */
+function clock(){
+    // Get the current time and day.
+    let d = new Date();
+
+    // Merge the date bits together.
+    let datetime = String(d.getDate()).padStart(2, '0');
+    datetime += ' ' + monthNames[d.getMonth()];
+    datetime += ' ' + String(d.getFullYear());
+    // Add the time bits.
+    datetime += ' ' + String(d.getHours()).padStart(2, '0');
+    datetime += ':' + String(d.getMinutes()).padStart(2, '0');
+    datetime += ':' + String(d.getSeconds()).padStart(2, '0');
+
+    // Add the timezone
+    datetime += ' '+timezone;
+
+    // Write these to the document.
+    writeToPage('dateTime', datetime);
+
+    // Call this function again in 100ms.
+    setTimeout(clock, 100);
+}
+
+/** Write a value to the document, padding with extra characters if required.
+ * @param {String} id the ID of the element to inject into.
+ * @param {String} val The value to print
+*/
+function writeToPage(id, val){
+    document.getElementById(id).innerHTML = val;
+}
+
